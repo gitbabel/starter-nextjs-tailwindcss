f4d94cb2e5a1557a7f274274a3d8e2d2de496bd4
plaetzaw <plaetzaw@gmail.com>
2021-09-02 08:48:54 -0500

    Added initial stories files and usercard, switching to brainch

---
+++ stories/organisms/UserCard.stories.js
@@ -0,0 +1,30 @@
+// import React from 'react'
+
+// import { withNextRouter } from 'storybook-addon-next-router'
+
+// import { STORYBOOK_SECTION } from '../constants'
+
+// import Component from '../../../pages/components/core/atoms/BabelMark'
+
+// const COMPONENT_NAME = 'BabelMark'
+
+// const MARKDOWN_DESCRIPTION = `
+// __${COMPONENT_NAME}__ is the original 32x44 Mark and is a white-fill that has lienar-gradients painted onto the three corresponding polygons
+
+// `
+
+// /*
+//  * Enable the META for the Component-Story
+//  */
+// export default {
+//   title: `${STORYBOOK_SECTION}/atoms/BabelMark`,
+//   parameters: {
+//     docs: {
+//       description: { component: MARKDOWN_DESCRIPTION }
+//     }
+//   },
+//   decorators: [withNextRouter]
+// }
+
+// // Base Component without the details enabled
+// export const Base = (args) => { return (<Component {...args} />) }
