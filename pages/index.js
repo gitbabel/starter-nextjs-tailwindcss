872fbd7a3570dd4119f826d813a488821d701232
plaetzaw <plaetzaw@gmail.com>
2021-09-08 10:07:16 -0500

    Added ApolloClient to the page, need to add GQL query

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,11 @@
 import Head from 'next/head'
+
+// Just need the query interfaces
+import { ApolloClient, ApolloProvider, gql, useQuery } from '@apollo/client'
+// ApolloClient REQUIRES a `cache` object
+import { cache } from '../lib/cache'
+
+// Organisms
 import CollectionsData from '../components/organisms/Hero/CollectionsData'
 import HeroComponent from '../components/organisms/Hero/HeroComponent'
 
@@ -15,17 +22,46 @@ const collectionspayload = {
   details: 'feat (initial)'
 }
 
-export default function Home () {
+export async function getStaticProps (context) {
+  console.log('Message at build-time')
+  return {
+    props: {
+      GRAPHQL_API_URL: process.env.GRAPHQL_API_URL,
+      API_BEARER_TOKEN: process.env.API_BEARER_TOKEN
+    }
+  }
+}
+
+export default function Home (props) {
+  // TODO: abstract queries into a lib/query folder, realizing we can create a shared package
+  const GRAPHQL_API_URL = props.GRAPHQL_API_URL
+  // console.log('WOW I HAVE URL? ', GRAPHQL_API_URL)
+  // Make a call to Apollo
+  const client = new ApolloClient({
+    uri: GRAPHQL_API_URL,
+    connectToDevTools: true,
+    // Allow 3rd Party (api-calls), requires CORS enablement
+    credentials: 'include',
+    // ApolloClient 3.x requires a cache object
+    // our cache is generated from ./cache.js
+    cache: cache,
+    headers: {
+      Authorization: `Bearer: ${props.API_BEARER_TOKEN}`
+    }
+
+  })
   return (
     <>
       <Head>
         <title>Create Next App</title>
         <link rel='icon' href='/favicon.ico' />
       </Head>
-      <div className='flex flex-col items-center align-items bg-[#29b5ed] p-4 h-screen'>
-        <HeroComponent {...heropayload} />
-        <CollectionsData {...collectionspayload} />
-      </div>
+      <ApolloProvider client={client}>
+        <div className='flex flex-col items-center align-items bg-[#29b5ed] p-4 h-screen'>
+          <HeroComponent {...heropayload} />
+          <CollectionsData {...collectionspayload} />
+        </div>
+      </ApolloProvider>
     </>
   )
 }
